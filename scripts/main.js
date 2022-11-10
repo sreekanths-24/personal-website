6a8405ea9c4ae9210f5ee7fd1c840545a7d8a83a
sreekanths-24 <sreekanthsanthosh17@gmail.com>
2022-11-10 16:09:21 +0530

    fixed the image

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,3 @@
-// const toggleButton = document.getElementsByClassName("toggle-button")[0]
-// const navbarLinks = document.getElementsByClassName("navbar-links")[0]
-
-// toggleButton.addEventListener('click', () => {
-//     navbarLinks.classList.toggle('active')
-// })
-
 const navSlide = () => {
     const burger = document.querySelector('.burger');
     const nav = document.querySelector('.nav-links');
