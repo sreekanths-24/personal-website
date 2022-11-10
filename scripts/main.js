94ec5e1a20bf65d1c5551684e8f575e4dfcee0bf
sreekanths-24 <sreekanthsanthosh17@gmail.com>
2022-11-10 17:16:11 +0530

    final fix with nav bar

--- scripts/main.js
+++ scripts/main.js
@@ -2,6 +2,7 @@ const navSlide = () => {
     const burger = document.querySelector('.burger');
     const nav = document.querySelector('.nav-links');
     const navLinks = document.querySelectorAll('.nav-links li');
+    
 
     burger.addEventListener('click', () =>  {
         //toggle nav
