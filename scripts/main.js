b9c73711237967d48d5d212a3808b2b93c4de3c8
sreekanths-24 <sreekanthsanthosh17@gmail.com>
2022-11-13 08:32:30 +0530

    added the parallax effect

--- scripts/main.js
+++ scripts/main.js
@@ -35,4 +35,12 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     });
 });
 
+//parallax effect
+const parallax = document.getElementById("parallax")
+
+window.addEventListener("scroll", function(){
+    let offset = window.pageYOffset;
+    parallax.style.backgroundPositionY = offset * 0.7 + "px";
+})
+
 navSlide();
