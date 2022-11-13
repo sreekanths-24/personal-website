ddf2afab9c3d7848f21ba9418cf7828a9f14c8d6
sreekanths-24 <sreekanthsanthosh17@gmail.com>
2022-11-13 15:54:00 +0530

    added typejs

--- scripts/main.js
+++ scripts/main.js
@@ -35,6 +35,26 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     });
 });
 
+//typed
+var typed = new Typed('.typing', {
+    strings: [
+        'am Sreekanth S.', 
+        'am a Front End Developer.', 
+        'am a Computer Science Engineer.',
+        'love learning new things.',
+        'am into Web Development.',
+        'am into Artificial Intelligence.',
+        'am into Cloud Computing.'
+    ],
+    smartBackspace: true,
+    typeSpeed: 60,
+    backSpeed: 20,
+    backDelay: 2000,
+    loop: true,
+    showCursor: true,
+    cursorChar: "_"
+  });
+
 //parallax effect
 // const parallax = document.getElementById("parallax")
 
