17e37ff834926221e13310fcc37ec108b5fa9cad
sreekanths-24 <sreekanthsanthosh17@gmail.com>
2022-11-17 19:49:35 +0530

    made some design changes

--- scripts/scroll-effects.js
+++ scripts/scroll-effects.js
@@ -1,19 +1,17 @@
 ScrollReveal().reveal(".cards",{
-    delay: 200,
     interval: 200,
     easing: 'ease-out',
     scale: 1.2,
     reset: true ,
-    duration: 1000
+    duration: 600
 });
 
 ScrollReveal().reveal(".ed-card",{
-    delay: 200,
     interval: 200,
     easing: 'ease',
     origin:'bottom',
     distance:'80px',
     reset: true,
-    duration: 1000
+    duration: 600
 });
 
