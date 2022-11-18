9e6ca506926fa4566dc5da385059b041b5f9cfac
sreekanths-24 <sreekanthsanthosh17@gmail.com>
2022-11-18 19:51:29 +0530

    just

--- scripts/main.js
+++ scripts/main.js
@@ -56,12 +56,4 @@ var typed = new Typed('.typing', {
   });
 
 
-//parallax effect
-// const parallax = document.getElementById("parallax")
-
-// window.addEventListener("scroll", function(){
-//     let offset = window.pageYOffset;
-//     parallax.style.backgroundPositionY = offset * 0.9 + "px";
-// })
-
 navSlide();
