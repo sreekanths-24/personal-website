70568672ffd905b20a205b76df0d8b9ff70c4788
sreekanths-24 <sreekanthsanthosh17@gmail.com>
2022-11-26 10:16:35 +0530

    fixed navbar for desktop

--- scripts/main.js
+++ scripts/main.js
@@ -27,3 +27,24 @@ var typed = new Typed('.typing', {
     showCursor: true,
     cursorChar: "_"
   });
+
+//scroll reveal
+ ScrollReveal().reveal(".cards",{
+    interval: 200,
+    easing: 'ease-out',
+    scale: 1.2,
+    reset: true ,
+    duration: 600,
+    desktop: false
+});
+
+ScrollReveal().reveal(".ed-card",{
+    interval: 200,
+    easing: 'ease',
+    origin:'bottom',
+    distance:'80px',
+    reset: true,
+    duration: 600,
+    desktop: false
+});
+
